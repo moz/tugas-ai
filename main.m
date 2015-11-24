86f3229ceac3555fa5fd04a1c6f187724837b1eb
Rahman Pujianto <rahman.pujianto@ui.ac.id>
2015-11-24 10:28:20 +0700

    menambah hingga hitung rbm2

--- main.m
+++ main.m
@@ -5,16 +5,16 @@ PS1('>> ');
 format long;
 
 % Load data dari file
-data = load('data.txt');
+data = load('data2.txt');
 x  = data(:,1);
-ei = data(:,2:7);
+%ei = data(:,2:7);
 
-num_input = 20;
+num_input = 5;
 
 %normalisasi
-%mi = min(x);
-%ma = max(x);
-%x = (x .- mi)./(ma-mi);
+mi = min(x);
+ma = max(x);
+x = (x .- mi)./(ma-mi);
 
 %mi = min(min(ei));
 %ma = max(max(ei));
@@ -34,18 +34,29 @@ for i=1:num_input
 end
 
 %vi = [xi ei(1:r,:)];
-vi = xi
+vi = xi;
 
 clear r c i akhir
 
-maxepoch = 1000; % maximum number of epochs
-numhid = 10;   % number of hidden units 
+maxepoch = 100; % maximum number of epochs
+numhid = 100;   % number of hidden units 
 batchdata = zeros(1, size(vi, 2), size(vi,1)); % the data that is divided into batches (numcases numdims numbatches)
 restart = 1;  % set to 1 if learning starts from beginning 
 
 for i=1:size(batchdata, 3)
-    data = vi(i,:);
+    data = vi(i:);
     batchdata(1, :, i) = data';
 end
 
+%{
+fprintf(1, 'RBM1 %d-%d\n', num_input,numhid);
 rbm
+w1=vishid;
+
+numhid =1;
+restart = 1;
+fprintf(1, 'RBM2 %d-%d\n', size(batchdata, 2), numhid);
+batchdata=batchposhidprobs;
+rbm
+w2=vishid;
+%}
